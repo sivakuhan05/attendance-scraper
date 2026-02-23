9f6d17e0f9ebc54e87bb11160c373812f0ba2da2
Sivakuhan <arsivakuhan@gmail.com>
2026-02-23 20:39:35 +0530

    feat: auto navigate to attendance page after login

--- scraper.py
+++ scraper.py
@@ -2,16 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 options = Options()
 options.binary_location = "/snap/bin/brave"
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--user-data-dir=/tmp/brave-selenium")
 
 driver = webdriver.Chrome(
         service=Service(ChromeDriverManager(driver_version="145").install()),
         options=options
         )
 
-driver.get("https://www.google.com")
+print("Driver created")
+
+driver.maximize_window()
+
+# Pull up login page and allow the user to login
+driver.get("https://ecampus.psgtech.ac.in/studzone")
+print("Login to account")
+time.sleep(10)
+
+# Pull up the attendance page once the user logs in
+driver.get("https://ecampus.psgtech.ac.in/studzone/Attendance/StudentPercentage")
+time.sleep(20)
+
+
